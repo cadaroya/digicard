c7447788a0e8907038e334ff5934bab3f2947fa2
Pauline Ocampo <plocampo1@up.edu.ph>
2018-02-09 00:46:28 +0800

    Added basic StudLog

---
+++ client/components/StudLog.vue
@@ -0,0 +1,42 @@
+<template>
+     <div>
+          <table>
+          <thead>
+               <tr>
+                    <th>Date</th>
+                    <th>Seat</th>
+                    <th>Timein</th>
+                    <th>Timeout</th>
+                    <th>Free Hours</th>
+               </tr>
+          </thead>
+          <tbody>
+               <!-- Key is supposed to be a unique identifier, 
+               I just added an arbitrary value to silence the warning. :)
+               Replace the value with the primary key once db is set up-->
+               <tr v-for="row in log" :key="row.date">
+                    <th>{{row.date}}</th>
+                    <th>{{row.seat}}</th>
+                    <th>{{row.timein}}</th>
+                    <th>{{row.timeout}}</th>
+                    <th>{{row.freehours}}</th>
+               </tr>
+          </tbody>
+          </table>
+     </div>
+</template>
+
+
+<script>
+export default {
+     props: ['studNo'],
+     data: function() {
+          return {
+               log: [
+                    {date: '23/11/17', seat: 12, timein: '12:00', timeout: '14:00', freehours: '14:50'},
+                    {date: '23/10/17', seat: 14, timein: '10:00', timeout: '11:00', freehours: '15:50'}
+               ]
+          }
+     }
+}
+</script>
--- client/views/Student.vue
+++ client/views/Student.vue
@@ -4,12 +4,15 @@
         <h1> Some Student's Page </h1>
         <p> His student number is {{ $route.params.studNo }} </p>
         <stud-info :studNo="$route.params.studNo"></stud-info>
+        <stud-log :studNo="$route.params.studNo"></stud-log>
     </div>
 </template>
 
 <script>
     import StudInfo from 'components/StudInfo'
+    import StudLog from 'components/StudLog.vue'
+
     export default {
-        components: { StudInfo }
+        components: { StudInfo, StudLog }
     }
 </script>
