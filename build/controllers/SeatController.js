0262651e5e3e47fafdd11667814542bea465523e
Carlos Daroya <daroyac@gmail.com>
2018-03-07 20:30:01 +0800

    merge conflict 2 timeincontroller

--- build/controllers/SeatController.js
+++ build/controllers/SeatController.js
@@ -26,18 +26,15 @@
 
 
 *     Code history:
-*     16/02/2018: 	File Created	                            Daroya, Carlos Adrian A.
-*     16/02/2018: 	TimeIn,Timeout cases	                    Daroya, Carlos Adrian A.
-*     21/02/2018: 	Null Checking       	                    Daroya, Carlos Adrian A.
-*     06/03/2018: 	Reflect change from student model         Daroya, Carlos Adrian A.
+*     07/03/2018: 	File Created               Daroya, Carlos Adrian A.
 *
 *
-*     Date created: 16 February 2018
+*     Date created: 07 March 2018
 *     Development Group: Cai, Daroya, Ocampo
 *    
 
 *     File purpose:
-*     Student controller
+*     Seat Controller
 */
 const {sequelize} = require('../models')
 const {student} = require('../models')
@@ -63,69 +60,18 @@ module.exports = {
                 // Select timestamp
                 const timeIN = (await sequelize.query("SELECT NOW() AS time" , {type: sequelize.QueryTypes.SELECT}))[0].time
 
-                /*
-                if(seat != null){
-                  // Create Report (timein only)
-                  const data = {
-                    rid: null,
-                    sno: stud.sno,
-                    freehours: stud.freehours,
-                    timein: timeIN,
-                    timeout: null,
-                    amountdue: null,
-                    seatNo: seat
-                  }
+                // Create Report (timein only)
+                const data = {
+                  rid: null,
+                  sno: stud.sno,
+                  freehours: stud.freehours,
+                  timein: timeIN,
+                  timeout: null,
+                  amountdue: null,
+                  seatNo: null
                 }
-                else{
-                  // Create Report (timein only)
-                  // Randomize if seat == null
-                  if(stud.preference.localeCompare(windows) == 0){
-                    // Retrieve seats with windows os && AVAILABLE
-                    const seat_windows = await sequelize.query("SELECT * FROM seat LEFT JOIN USING seatno report WHERE os = ? AND timeout IS NOT NULL" , {replacements: [windows],type: sequelize.QueryTypes.SELECT})
-                    // Randomize from AVAILABLE windows computers
-                    const max = seat_windows.length - 1
-                    const min = 0
-                    seat = Math.floor(Math.random() * (max - min + 1)) + min
-                      
-                    // Get the i*th element (seat) from seat_windows
-                    seat = seat_windows[seat].seatno
-                  }
-                  else{
-                    // Retrieve seats with mac os && AVAILABLE
-                    const seat_mac = await sequelize.query("SELECT * FROM seat LEFT JOIN USING seatno report WHERE os = ? AND timeout IS NOT NULL" , {replacements: [mac],type: sequelize.QueryTypes.SELECT})
-                    // Randomize from AVAILABLE mac computers
-                    const max = seat_mac.length - 1
-                    const min = 0
-                    seat = Math.floor(Math.random() * (max - min + 1)) + min
-                      
-                    // Get the i*th element (seat) from seat_mac
-                    seat = seat_mac[seat].seatno
-                    
-                  }
-                  // Create report with random seatno
-                  const data = {
-                    rid: null,
-                    sno: stud.sno,
-                    freehours: stud.freehours,
-                    timein: timeIN,
-                    timeout: null,
-                    amountdue: null,
-                    seatNo: seat
-                  }                  
-                }
-                */
 
-               const data = {
-                rid: null,
-                sno: stud.sno,
-                freehours: stud.freehours,
-                timein: timeIN,
-                timeout: null,
-                amountdue: null,
-                seatNo: null
-              }    
-
-              report.create(data)
+                report.create(data)
 
                 // Update student session to 1
                 await sequelize.query("UPDATE student SET session = 1 WHERE sno = ?" , { replacements: [search], type: sequelize.QueryTypes.UPDATE})
